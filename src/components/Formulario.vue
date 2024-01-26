6cd94ff59e44d52b90cd7ad75a929576afd914f7
Edson733 <105471845+Edson733@users.noreply.github.com>
2024-01-25 23:53:35 -0600

    Formulario Terminado

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,74 +1,141 @@
 <template>
-  <div class="py-5 px-5">
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="input-group-1" label="Nombre(s):" label-for="input-1">
-        <b-form-input id="input-1" v-model="form.nombre" placeholder="Coloca tu nombre(s)" required></b-form-input>
-      </b-form-group>
-      <b-form-group id="input-group-2" class="mt-3" label="Apellido Paterno" label-for="input-2">
-        <b-form-input id="input-2" v-model="form.apaterno" placeholder="Coloca tu apellido paterno" required></b-form-input>
-      </b-form-group>
-      <b-form-group id="input-group-3" class="mt-3" label="Apellido Materno:" label-for="input-3">
-        <b-form-input id="input-3" v-model="form.amaterno" placeholder="Coloca tu apellido materno"></b-form-input>
-      </b-form-group>
-      <b-form-group id="input-group-4" class="mt-3" label="Direccion Completa:" label-for="input-4">
-        <b-form-input id="input-4" v-model="form.direccion" placeholder="Coloca tu direccion"></b-form-input>
-      </b-form-group>
-      <b-form-group id="input-group-5" class="mt-3" label="Fecha de Nacimiento:" label-for="input-5">
-        <b-form-datepicker id="example-datepicker" v-model="value" placeholder="Coloca tu fecha de nacimiento" class="mb-2"></b-form-datepicker>
-      </b-form-group>
-      <b-form-group id="input-group-6" class="mt-3" label="Correo Electronico:" label-for="input-6">
-        <b-form-input id="input-6" v-model="form.email" type="email" placeholder="Coloca tu email" required></b-form-input>
-      </b-form-group>
-      <b-form-group id="input-group-7" class="mt-3" label="Numero Telefonico:" label-for="input-7">
-        <b-form-input id="input-7" v-model="form.phone" placeholder="Coloca tu numero telefonico"></b-form-input>
-      </b-form-group>
-      <b-form-group id="input-group-8" class="mt-3" label="Selecciona una Fotografia:" label-for="input-8">
-        <b-form-file v-model="image" plain></b-form-file>
-      </b-form-group>
-
-      <b-button class="mt-3" type="submit" variant="primary">Submit</b-button>
-      <b-button class="mt-3" type="reset" variant="danger">Reset</b-button>
-    </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
-  </div>  
+  <b-conteiner>
+    <div class="py-5 px-5">
+      <h1 class="text-center">Formulario</h1>
+      <b-card>
+        <b-form @submit.prevent="validarForm" action="https://vuejs.org/" method="post" novalidate>
+          <b-alert variant="danger" dismissible :show="errors.length > 0">
+            <b>{{ errors.length > 1 ? "Por favor corrige los siguientes errores: " : "Por favor corrige el siguiente error: " }}</b>
+            <ul>
+              <li v-for="error in errors" :key="error">{{ error }}</li>
+            </ul>
+          </b-alert>
+          <b-form-group id="nombre-group" label="Nombre(s):" label-for="nombre" :state="nombre ? null : false">
+            <b-form-input id="nombre" v-model="nombre" type="text" required></b-form-input>
+          </b-form-group>
+          <b-form-group id="apellidoP-group" class="mt-3" label="Apellido Paterno" label-for="apellidoP" :state="apellidoP ? null : false">
+            <b-form-input id="apellidoP" v-model="apellidoP" type="text" required></b-form-input>
+          </b-form-group>
+            <b-form-group id="apellidoM-group" class="mt-3" label="Apellido Materno:" label-for="apellidoM">
+          <b-form-input id="apellidoM" v-model="apellidoM"></b-form-input>
+            </b-form-group>
+          <b-form-group id="direccion-group" class="mt-3" label="Direccion Completa:" label-for="direccion">
+            <b-form-input id="direccion" v-model="direccion"></b-form-input>
+          </b-form-group>
+          <b-form-group id="nacimiento-group" class="mt-3" label="Fecha de Nacimiento:" label-for="nacimiento" :state="validarEdad ? null : false">
+            <b-form-datepicker id="nacimiento" v-model="nacimiento" type="date" name="nacimiento" class="mb-2" required></b-form-datepicker>
+          </b-form-group>
+          <b-form-group id="email-group" class="mt-3" label="Correo Electronico:" label-for="email" :state="validarEmail(email) ? null : false">
+            <b-form-input id="email" v-model="email" type="email" name="email" required></b-form-input>
+          </b-form-group>
+          <b-form-group id="numero-group" class="mt-3" label="Numero Telefonico:" label-for="numero" :state="validarNumero(numero) ? null : false">
+            <b-form-input id="numero" v-model="numero" type="tel" name="numero" required></b-form-input>
+          </b-form-group>
+          <b-form-group id="imagen-group" class="mt-3" label="Selecciona una Fotografia (PNG):" label-for="imagen" :state="validarImagen ? null : false">
+            <b-form-file id="imagen" v-model="imagen" type="file" accept=".png" @change="handleFileChange" required></b-form-file>
+          </b-form-group>
+          <b-button class="mt-3" type="submit" variant="success">Enviar</b-button>
+        </b-form>
+      </b-card>
+    </div>
+  </b-conteiner>
 </template>
 
 <script>
-  export default {
+  import Vue from "vue";
+
+  export default Vue.extend({
     data() {
       return {
-        form: {
-          email: '',
-          name: '',
-          food: null,
-          checked: []
-        },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
-      }
+        errors: [],
+        nombre: null,
+        apellidoP: null,
+        apellidoM: null,
+        direccion: null,
+        nacimiento: null,
+        email: null,
+        numero: null,
+        imagen: null,
+        validarEdad: false,
+        validarImagen: false,
+      };
     },
     methods: {
-      onSubmit(event) {
-        event.preventDefault()
-        alert(JSON.stringify(this.form))
+      validarForm: function (e) {
+        this.errors = [];
+
+        if (!this.nombre) {
+          this.errors.push("Nombre obligatorio.");
+        }
+
+        if (!this.apellidoP) {
+          this.errors.push("Apellido Paterno obligatorio.");
+        }
+
+        if (!this.direccion) {
+          this.errors.push("Direccion obligatoria.");
+        }
+
+        if (!this.nacimiento) {
+          this.errors.push("Fecha de Nacimiento obligatoria.");
+        } else {
+          const hoy = new Date();
+          const fechaNacimiento = new Date(this.nacimiento);
+          const edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+          const diferenciaMes = hoy.getMonth() - fechaNacimiento.getMonth();
+          if (diferenciaMes < 0 || (diferenciaMes === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
+            this.validarEdad = edad - 1;
+          } else {
+            this.validarEdad = edad;
+          }
+          if (this.validarEdad < 18) {
+            this.errors.push("La edad debe ser 18 o más.");
+          }
+        }
+
+        if (!this.email) {
+          this.errors.push("Correo Electrónico obligatorio.");
+        } else if (!this.validarEmail(this.email)) {
+          this.errors.push("Por favor ingresa una dirección de correo válida.");
+        }
+
+        if (!this.numero || this.numero.length < 10) {
+          this.errors.push("El número de teléfono debe tener al menos 10 caracteres.");
+        }
+
+        if (!this.imagen || !this.validarImagen) {
+          this.errors.push("La foto es obligatoria y debe tener menos de 3 MB.");
+        }
+
+        if (!this.errors.length) {
+          return true;
+        }
+
+        e.preventDefault();
       },
-      onReset(event) {
-        event.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
+      validarEmail: function (email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      },
+      validarNumero: function (numero) {
+        return true;
+      },
+      handleFileChange(event) {
+        const fileInput = event.target;
+        if (fileInput.files.length > 0) {
+          const file = fileInput.files[0];
+          const tamañoMaximoMB = 3;
+
+          if (file.size > tamañoMaximoMB * 1024 * 1024) {
+            this.validarImagen = false;
+            this.errors.push(`El tamaño de la foto debe ser menos de ${tamañoMaximoMB} MB.`);
+          } else {
+            this.validPhoto = true;
+          }
+        }
+      },
     }
-  };
+  });
 </script>
 
 <style></style>
